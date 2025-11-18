32dea35892e69fa3d3f05024179bb81357f25177
radian <12453432+radian-jp@users.noreply.github.com>
2025-11-18 21:40:49 +0900

    Add sample pattern

--- Program.cs
+++ Program.cs
@@ -12,14 +12,18 @@ unsafe
 {
     fixed (byte* pUTF16 = &bytesUTF16[0], pUTF8 = &bytesUTF8[0], pSJIS = &bytesSJIS[0])
     {
+        Console.WriteLine($"source:{str}");
         Console.WriteLine($"Marshal.PtrToStringUni(UTF-16):{Marshal.PtrToStringUni((IntPtr)pUTF16)}");
         Console.WriteLine($"Marshal.PtrToStringUni(UTF-8):{Marshal.PtrToStringUni((IntPtr)pUTF8)}");
         Console.WriteLine($"Marshal.PtrToStringUni(Shift-JIS):{Marshal.PtrToStringUni((IntPtr)pSJIS)}");
         Console.WriteLine($"Marshal.PtrToStringAnsi(UTF-16):{Marshal.PtrToStringAnsi((IntPtr)pUTF16)}");
         Console.WriteLine($"Marshal.PtrToStringAnsi(UTF-8):{Marshal.PtrToStringAnsi((IntPtr)pUTF8)}");
         Console.WriteLine($"Marshal.PtrToStringAnsi(Shift-JIS):{Marshal.PtrToStringAnsi((IntPtr)pSJIS)}");
-        Console.WriteLine($"EncodingDetector.DecodeAuto(UTF-16):{EncodingDetector.DecodeAuto(bytesUTF16)}");
-        Console.WriteLine($"EncodingDetector.DecodeAuto(UTF-8):{EncodingDetector.DecodeAuto(bytesUTF8)}");
-        Console.WriteLine($"EncodingDetector.DecodeAuto(Shift-JIS):{EncodingDetector.DecodeAuto(bytesSJIS)}");
+        Console.WriteLine($"EncodingDetector.DecodeAuto(Span UTF-16):{EncodingDetector.DecodeAuto(bytesUTF16)}");
+        Console.WriteLine($"EncodingDetector.DecodeAuto(Span UTF-8):{EncodingDetector.DecodeAuto(bytesUTF8)}");
+        Console.WriteLine($"EncodingDetector.DecodeAuto(Span Shift-JIS):{EncodingDetector.DecodeAuto(bytesSJIS)}");
+        Console.WriteLine($"EncodingDetector.DecodeAuto(pointer UTF-16):{EncodingDetector.DecodeAuto(pUTF16, 100)}");
+        Console.WriteLine($"EncodingDetector.DecodeAuto(pointer UTF-8):{EncodingDetector.DecodeAuto(pUTF8, 100)}");
+        Console.WriteLine($"EncodingDetector.DecodeAuto(pointer Shift-JIS):{EncodingDetector.DecodeAuto(pSJIS, 100)}");
     }
 }
